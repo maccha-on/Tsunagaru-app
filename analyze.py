872ddbaaf3b2f7bcc4dc709bf75405ce386820ed
maccha-on <masato.ohtani.tech0@gmail.com>
2025-09-23 12:31:03 +0900

    修復中エラー回避の分岐を作成

--- analyze.py
+++ analyze.py
@@ -28,16 +28,18 @@ def find_major_commons(name, client):
     try:
         # Cloud環境なら st.secrets["DATA"] が使える
         data_txt = st.secrets["MEMBER_DATA"]
-        print('secretsから取得')
+        st.sidebar.caption('secretsから取得中...')
     except Exception:
-        data_txt = str(pd.read_csv("out.csv"))
-        print('out.csvから取得')
+        p = Path(__file__).parent / "out.csv"
+        if p.exists():
+            data_txt = str(pd.read_csv("out.csv"))
+            st.write('ローカルcsvから取得中...')
          
     # ChatGPTを呼び出しスクリプト
     request_to_gpt = (
-        f"「{name}」が持つ特徴について、多くの人と共通するものを最大３つほど教えてください。"
+        f"「{name}」が持つ特徴について、他の多くのメンバーとも共通するものを最大３つほど教えてください。"
         f"各共通点については、誰と共通しているのかも説明してください。"
-        f"回答は、最大でも300字程度にしてください。"
+        f"回答は、前置き無しで結論を記載。最大でも300字程度にしてください。\n"
         f"メンバーとその特徴は、以下のとおりです。\n\n"
         f"{data_txt}"
         )
@@ -63,18 +65,23 @@ def find_similar_person(name, client):
     try:
         # Cloud環境なら st.secrets["DATA"] が使える
         data_txt = st.secrets["MEMBER_DATA"]
-        print('secretsから取得')
+        st.sidebar.caption('secretsから取得中...')
     except Exception:
-        data_txt = str(pd.read_csv("out.csv"))
-        print('out.csvから取得')
-
+        p = Path(__file__).parent / "out.csv"
+        if p.exists():
+            data_txt = str(pd.read_csv("out.csv"))
+            st.sidebar.caption('ローカルcsvから取得中...')
+         
         
     # ChatGPTを呼び出しスクリプト
     request_to_gpt = (
-        f"「{name}」と、多くの共通点がある人を2～3人教えてください。"
+        f"「{name}」と、多くの共通点がある人を3人教えてください。"
         f"回答形式については、共通点の多いメンバーの名前を書き、共通や類似するポイントを箇条書きで教えてください。"
-        f"回答は、最大でも300文字程度としてください。"
-        f"メンバーとその特徴は、以下のとおりです。\n\n"
+        f"回答は、最大でも300文字程度としてください。\n"
+        f"#回答例のイメージ\n"
+        f"1人目: たっちゃん\n"
+        f"理由: xxxxx \n\n"
+        f"#メンバーとその特徴リスト\n"
         f"{data_txt}"
         )
     # 決めた内容を元にchatGPTへリクエスト

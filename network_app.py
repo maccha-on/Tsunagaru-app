98353ac9a4220450f7949d51bec4ceb730b65f3b
maccha-on <masato.ohtani.tech0@gmail.com>
2025-10-03 04:22:51 +0900

    クラウド環境の判定条件を修正／名前からニックネームだけを抽出する処理を追加

--- network_app.py
+++ network_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import networkx as nx
 from pyvis.network import Network
 import streamlit as st
+import os
 
 # --------------------------------------------------------
 # 固定ファイル名（JSON）
